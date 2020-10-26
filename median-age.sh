480bf337a2f38e1097a58b74219afa8396599ed7
Nic McPhee <mcphee@morris.umn.edu>
2020-10-26 15:39:26 -0500

    Allow calls in other directories
    This failed if the call didn't happen in the same directory (or somewhere in that repository) as file we were trying to compute the median age of. This fixes that by `cd`ing into the directory containing the file, and then calling `git blame`.
    
    I put all this in a scope (using parentheses) so that we can `cd` into the new directory and not need to worry about `cd`ing back to the initial directory.

--- median-age.sh
+++ median-age.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-file=$1
+path=$1
+dir=$(dirname "$path")
+file=$(basename "$path")
 
-git blame --line-porcelain "$file" | \
-   # We want to get the author-time lines out of `git blame --porcelain`. \
-   grep author-time | \
-   # Take the median of the 2nd column (the time stamps) \
-   datamash --field-separator ' ' median 2
-
+(
+    cd "$dir" || exit
+    git blame --line-porcelain "$file" | \
+        # We want to get the author-time lines out of `git blame --porcelain`. \
+        grep author-time | \
+        # Take the median of the 2nd column (the time stamps) \
+        datamash --field-separator ' ' median 2
+)
